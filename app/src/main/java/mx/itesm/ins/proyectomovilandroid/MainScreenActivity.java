295137d20c4b9a4854dc90d66e3f4fe72187ddea
Ian <ianneumann9@gmail.com>
2018-03-06 22:31:04 -0600

    Added Navigation Functionality, And added basic project card

--- app/src/main/java/mx/itesm/ins/proyectomovilandroid/MainScreenActivity.java
+++ app/src/main/java/mx/itesm/ins/proyectomovilandroid/MainScreenActivity.java
@@ -3,28 +3,42 @@ package mx.itesm.ins.proyectomovilandroid;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
-public class MainScreenActivity extends AppCompatActivity {
+import java.util.Calendar;
 
-    private TextView mTextMessage;
+public class MainScreenActivity extends AppCompatActivity {
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment emptyFrag = new Fragment();
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                case R.id.navigation_profile:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragmentPlacer, emptyFrag).commit();
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                case R.id.navigation_browse:
+                    Fragment card = new ProjectCard();
+                    Bundle args = new Bundle();
+                    args.putParcelable(ProjectCard.ARG_PROJECT,new Project(
+                            "Super Titulo",
+                            "https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/2000px-Google_%22G%22_Logo.svg.png",
+                            new String[]{ "Programador" },
+                            "Super descripcion",
+                            "La Condesa",
+                            Calendar.getInstance().getTime(),
+                            Calendar.getInstance().getTime())
+                    );
+                    card.setArguments(args);
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragmentPlacer, card).commit();
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragmentPlacer, emptyFrag).commit();
                     return true;
             }
             return false;
@@ -36,7 +50,6 @@ public class MainScreenActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
