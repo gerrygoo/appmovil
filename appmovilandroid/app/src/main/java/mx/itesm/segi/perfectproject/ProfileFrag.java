05ca4a26bdaf80da9c3ee9eb4c5e0d8296a67057
Ian Neumann <ianneumann9@gmail.com>
2018-03-31 15:22:01 -0600

    Added toggle accont functionality and fixed a couple of bugs with the draggable view messing with other views

--- appmovilandroid/app/src/main/java/mx/itesm/segi/perfectproject/ProfileFrag.java
+++ appmovilandroid/app/src/main/java/mx/itesm/segi/perfectproject/ProfileFrag.java
@@ -1,11 +1,14 @@
 package mx.itesm.segi.perfectproject;
 
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Switch;
 
@@ -19,11 +22,22 @@ public class ProfileFrag extends Fragment {
     private EditText tvCompany;
     private EditText tvCurriculum;
     private Switch Mode;
+    private OnSwitchToggleListener listener;
 
     public ProfileFrag() {
         // Required empty public constructor
     }
 
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        Activity activity = getActivity();
+        if(activity instanceof OnSwitchToggleListener){
+            listener = (OnSwitchToggleListener) activity;
+        } else {
+            throw new ClassCastException(activity.toString() + " must implement OnSwitchToggleListener");
+        }
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -35,6 +49,12 @@ public class ProfileFrag extends Fragment {
         tvCurriculum = v.findViewById(R.id.etCurriculum);
         tvCurriculum.setKeyListener(null);
         Mode = v.findViewById(R.id.sEmployer);
+        Mode.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                listener.OnSwitchToggle(b);
+            }
+        });
         loadProfileInfo();
         return v;
     }
@@ -43,5 +63,10 @@ public class ProfileFrag extends Fragment {
         tvName.setText(getArguments().getString("name"));
         tvCompany.setText(getArguments().getString("company"));
         tvCurriculum.setText(getArguments().getString("curriculum"));
+        Mode.setChecked(getArguments().getBoolean("mode"));
+    }
+
+    public interface OnSwitchToggleListener {
+        void OnSwitchToggle(boolean value);
     }
 }
