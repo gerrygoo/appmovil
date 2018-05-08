e9889ea346e8a906a43a475b03c3a52ae228960b
Santiago <A01371036@itesm.mx>
2018-05-08 16:19:30 -0500

    Regresa Utils!

---
+++ appmovilandroid/app/src/main/java/Model/Utils.java
@@ -0,0 +1,13 @@
+package Model;
+
+import android.graphics.Bitmap;
+import android.util.Log;
+
+public class Utils {
+public static Bitmap bitmapToThumbnail(Bitmap bitmap){
+    double ratio = 150.0 / Math.max(bitmap.getHeight(), bitmap.getWidth());
+    Log.e("New Values", "x: " + (int) (bitmap.getWidth() * ratio) + ", y: " + (int) (bitmap.getHeight() * ratio));
+
+    return Bitmap.createScaledBitmap(bitmap, (int) (bitmap.getWidth() * ratio), (int) (bitmap.getHeight() * ratio), false);
+    }
+}
